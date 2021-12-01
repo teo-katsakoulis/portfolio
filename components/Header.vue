9bfd38ca446c0d9f19704bbbdad3a3e54a7b7781
theo811 <theo811@hotmail.com>
2021-12-01 22:32:59 +0100

    Working page with vue particles

---
+++ components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1 class="red">Hello this is header</h1>
+    </div>
+</template>
---
+++ components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="header">
+    <div id="app">
+        <vue-particles color="#000"></vue-particles>
+    </div>
+    <div class="logo">
+      <nuxt-link to="/">
+        <Logo />
+      </nuxt-link>
+    </div>
+    <nav class="nav">
+      <div class="nav__link">
+        <nuxt-link to="/">Home</nuxt-link>
+      </div>
+      <div class="nav__link">
+        <nuxt-link to="/About">About</nuxt-link>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+    import Logo from "@/components/NuxtLogo";
+    export default {
+        name: "NavBar",
+        components: { Logo }
+    };
+</script>

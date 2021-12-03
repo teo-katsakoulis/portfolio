400d9717ddbf356a09981f3e250214da34e0742c
theo811 <theo811@hotmail.com>
2021-12-03 17:25:37 +0100

    Vue particles test

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,8 +23,7 @@ export default {
   css: ["~/assets/styles/main.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: ['~/plugins/vue-particles.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,

eb12742b08b44a0754fe6268ffcf81ca35ec78d7
theo811 <theo811@hotmail.com>
2021-12-06 21:09:42 +0100

    Add sidebar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,28 +1,35 @@
 <template>
-  <header class="header">
-    <div id="app">
-        <vue-particles color="#000"></vue-particles>
+  <nav>
+    <span class="brand">
+      <a href="#home">T</a>
+    </span>
+    <div class="menu">
+      <a href="#home" class="active scroll-page"><i class="material-icons">home</i></a><br>
+      <a href="#about" class="scroll-page"><i class="material-icons">account_circle</i></a><br>
+      <a href="#skills" class="scroll-page"><i class="material-icons">settings</i></a><br>
+      <a href="#works" class="scroll-page"><i class="material-icons">work</i></a><br>
+      <a href="#contact" class="scroll-page"><i class="material-icons">mail</i></a><br>
     </div>
-    <div class="logo">
-      <nuxt-link to="/">
-        <Logo />
-      </nuxt-link>
+    <div class="social">
+      <a href="www.linkedin.com/in/theofilos-katsakoulis" target="_blank">
+        <svg aria-hidden="true" data-prefix="fab" data-icon="linkedin" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="linkedin-icon">
+          <path fill="currentColor" d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"></path>
+        </svg>
+      </a>
+      <a href="https://www.xing.com/profile/Theofilos_Katsakoulis" target="_blank">
+        <svg aria-hidden="true" data-prefix="fab" data-icon="xing" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="svg-inline--fa fa-xing fa-w-12">
+          <path fill="currentColor" d="M162.7 210c-1.8 3.3-25.2 44.4-70.1 123.5-4.9 8.3-10.8 12.5-17.7 12.5H9.8c-7.7 0-12.1-7.5-8.5-14.4l69-121.3c.2 0 .2-.1 0-.3l-43.9-75.6c-4.3-7.8.3-14.1 8.5-14.1H100c7.3 0 13.3 4.1 18 12.2l44.7 77.5zM382.6 46.1l-144 253v.3L330.2 466c3.9 7.1.2 14.1-8.5 14.1h-65.2c-7.6 0-13.6-4-18-12.2l-92.4-168.5c3.3-5.8 51.5-90.8 144.8-255.2 4.6-8.1 10.4-12.2 17.5-12.2h65.7c8 0 12.3 6.7 8.5 14.1z"></path>
+        </svg>  
+      </a>
     </div>
-    <nav class="nav">
-      <div class="nav__link">
-        <nuxt-link to="/">Home</nuxt-link>
-      </div>
-      <div class="nav__link">
-        <nuxt-link to="/About">About</nuxt-link>
-      </div>
-    </nav>
-  </header>
+  </nav>
 </template>
 
 <script>
-    import Logo from "@/components/NuxtLogo";
     export default {
         name: "NavBar",
-        components: { Logo }
+        mounted() {
+            
+        }
     };
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app" class="relative">
-      <vue-particles color="#fff"
+    <NavBar/>
+    <vue-particles color="#fff"
                     :particleOpacity="0.5"
                     :particlesNumber="25"
                     shapeType="circle"
@@ -20,7 +21,7 @@
     <div class="main-container flex items-top justify-center bg-gray-100 sm:items-center sm:pt-0">
       <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
         <div class="main-title pt-8 sm:pt-0">
-          <h1>Hello world, I 'm Theo <br> I design & build</h1>
+          <h1>Hello world, I'm Theo. <br> I design & build</h1>
           <h2>
             <vue-typer
                   :text='["websites","applications","user interfaces"]'
@@ -29,7 +30,7 @@
                   initial-action='typing'
                   :pre-type-delay='70'
                   :type-delay='70'
-                  :pre-erase-delay='3000'
+                  :pre-erase-delay='2500'
                   :erase-delay='250'
                   erase-style='select-all'
                   :erase-on-complete='false'
@@ -37,19 +38,20 @@
                 ></vue-typer>
           </h2>
         </div>
-        <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
+        <!-- <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
           <h2 class="text-2xl leading-7 font-semibold">Welcome</h2>
           <p class="mt-3 text-gray-600">to my</p>
           <p class="mt-4 pt-4 text-gray-800 border-t border-dashed">website</p>
-        </div>
+        </div> -->
       </div>
     </div> 
   </div>
 </template>
 
 <script>
+  import NavBar from "@/components/NavBar";
   export default {
     name: "Index",
-    components: { }
+    components: { NavBar }
   };
 </script>

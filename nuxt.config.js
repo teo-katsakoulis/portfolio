23ba838f903ea772766cec3acfa3b4963ad73e0e
theo811 <theo811@hotmail.com>
2022-01-04 12:35:44 +0100

    Update nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,7 +58,6 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/bootstrap
-    'bootstrap-vue/nuxt',
     'nuxt-material-design-icons-iconfont'
   ],
 
